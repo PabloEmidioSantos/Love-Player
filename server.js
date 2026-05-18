d3d0dd5177ae24785bfd09b24fa2ea404f060928
Pablo Emidio <pabloemidiosantos28@gmail.com>
2026-05-18 16:27:33 -0300

    add

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 1905;
 //Isso aqui fala pro back que os arquivos estão na msm pasta
-app.use(express.static('./frontend'));
+//app.use(express.static('./frontend'));
 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/frontend/index.html');
 });
-
+//
 app.listen(port, () => {
   console.log(`Seu servidor esta rodando em: http://localhost:${port}`);
 });
